ee6729b4c684a830df35027b7f25ad2423c4b898
adamwexler01 <adamwexler01@ufl.edu>
2017-10-10 12:35:01 -0400

    making changes to get facebook api results

--- gather/ViewController.swift
+++ gather/ViewController.swift
@@ -22,10 +22,13 @@ class ViewController: UIViewController {
             //User already has access to the api/connected to facebook
             //So we can just generate the account information from
             //the facebook api
+            loginFBData()
+            referenceFBData()
+            
         } else {
             let button = FBSDKLoginButton()
             button.sizeThatFits(CGSize(width: CGFloat(100), height: CGFloat(50)))
-            button.center = self.view.center;
+            button.center = CGPoint(x: self.view.frame.midX, y: self.view.frame.size.height*9/10)
             self.view.addSubview(button)
         }
         
@@ -36,8 +39,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func referenceFBData() {
+        let request = FBSDKGraphRequest(graphPath:"me", parameters: ["fields":"email,first_name,gender"]);
+        
+        request?.start(completionHandler: { (connection, result, error) in
+            if(error != nil){
+                print("This is the result that you are looking for: \(String(describing: result))")
+            } else {
+                print("This is the seen error: \(String(describing: error))")
+            }
+        })
+    }
     
-    
+    func loginFBData(){
+        let faceBookLoginManger = FBSDKLoginManager()
+        
+        faceBookLoginManger.logIn(withReadPermissions: ["public_profile", "email", "user_friends"], from: self) { (result, error) in
+            if (error != nil)
+            {
+                print("error is \(error)")
+            }
+            if (result?.isCancelled)!
+            {
+                //handle cancelations
+            }
 
+        }
+        
+    }
 }
 
